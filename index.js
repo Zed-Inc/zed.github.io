96c9f13db5950d9045c53ec644e4ae4daaad1680
Zoran-Bailey <s3775574@student.rmit.edu.au>
2020-12-02 13:23:39 +1100

    small changes

---
+++ index.js
@@ -0,0 +1,30 @@
+
+
+
+/*
+
+Personal information:
+Zoran bailey
+s3775574
+s3775574@student.rmit.edu.au
+
+Fact: I really enjoy racing rc cars and printing
+
+questions:
+
+- what is your interest in IT? when did your interest in IT start?
+  Was there a paticular event or person that sparked your interest? outline your it experience(if any)
+
+
+
+
+
+- Why did you choose to come to RMIT
+
+- what do you expect to learn during your studies?
+
+
+
+
+
+*/
